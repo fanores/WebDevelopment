4a26a3542177607a31b514be2b3a8e226b4ad1fa
Stefan Misik <fanores@gmail.com>
2019-10-05 21:00:10 +0200

    JavaScript: string slice, 2nd example

--- JavaScript/stringSlice.js
+++ JavaScript/stringSlice.js
@@ -5,3 +5,8 @@ var firstCharUpperCase = firstChar.toUpperCase();
 var restChars = userNameInput.slice(1,userNameInput.length);
 var restCharsLowerCase = restChars.toLowerCase();
 alert("Your name is: " + firstCharUpperCase + restCharsLowerCase);
+
+/* Another option. */
+var userNameInput2 = prompt("Type your name:");
+userNameInput2.slice(0,1) = userNameInput2.slice(0,1).toUpperCase;
+alert("Your name is: " + userNameInput2);
