46f87275eb52b7c15ae38f8507316c6e3533487e
Stefan Misik <fanores@gmail.com>
2019-10-05 20:58:30 +0200

    JavaScript: string length

---
+++ JavaScript/stringLength.js
@@ -0,0 +1,4 @@
+/* Determine user's string length input. */
+var maxCharacters = 100;
+var mySentence = prompt("Type your sentence:");
+alert("You have written: " + mySentence.length + " characters out of: " + (maxCharacters - mySentence.length));
