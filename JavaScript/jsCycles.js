f79aa47a0c9c9f4ef2b6330d725213c8b883a02f
Stefan Misik <fanores@gmail.com>
2019-10-05 21:03:06 +0200

    JavaScript: cycles, while example

--- JavaScript/jsCycles.js
+++ JavaScript/jsCycles.js
@@ -2,3 +2,17 @@
 for (var i = 1; i < 6; i++) {
     console.log(i);
 }
+
+/* While example on the bottle song. */
+var numberOfBottles = 10
+while (numberOfBottles > 0) {
+    var bottleWord = "bottles";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottle";
+    }
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
