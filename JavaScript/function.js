673df0db5242de560d26c15f73587d0b70b4d51f
Stefan Misik <stefan.misik@sap.com>
2019-10-06 21:45:36 +0200

    JavaScript: another function call example

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -4,3 +4,14 @@ function printSomething() {
 }
 
 printSomething();
+
+
+/* Another example. */
+function getMilk(money) {
+  var numberOfBottles = Math.floor(money / 1.5);
+  console.log("you bought " + numberOfBottles + " bottle of milk");
+  return money % 1.5;
+}
+
+var change = getMilk(2);
+console.log("your change is " + change);
