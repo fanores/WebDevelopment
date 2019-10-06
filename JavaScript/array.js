0f3968c6e3ff5ef5e190eacfaddf48ad403a5c15
Stefan Misik <stefan.misik@sap.com>
2019-10-06 21:47:20 +0200

    JavaScript: array include function

---
+++ JavaScript/array.js
@@ -0,0 +1,10 @@
+/* Working with arrays. */
+var nameList = ["Stevo", "Simon"];
+var userName = prompt("Enter your name:");
+
+
+if (nameList.includes(userName)) {
+    console.log("Welcome.");
+} else {
+    console.log("Not Welcome.");
+}
