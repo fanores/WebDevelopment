d60184c1bc939d20a76dcebfa1af59de8d8c516a
StefanMisik <stefan.misik@sap.com>
2019-10-09 20:01:17 +0200

    DOM: selecting HTML elements

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,5 @@
+/* Change the last item's text in the list to: Stevo */
 document.querySelector("body").querySelector("ul").lastElementChild.innerHTML = "Stevo";
+
+/* Change the item in the list that contains ahref element to red color. */
+document.querySelector("li a").style.color = "red";
