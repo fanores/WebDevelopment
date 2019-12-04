dcaa38eda7c96435d6946e0cfd6087495c4e5499
Stefan Misik <fanores@gmail.com>
2019-12-04 22:52:03 +0100

    js_dom: adding event listener to a button

---
+++ JS_DOM/index.js
@@ -0,0 +1,6 @@
+// add an event listener for the first button
+document.querySelector("button").addEventListener("click", handleButtonClick);
+
+function handleButtonClick() {
+  alert("I got clicked!");
+}
