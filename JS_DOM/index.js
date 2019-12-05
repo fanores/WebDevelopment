6afa25b73cb0d6a4aff3d00a35e24acd06640006
Stefan Misik <fanores@gmail.com>
2019-12-05 16:56:32 +0100

    js_dom: play sound and change element text

--- JS_DOM/index.js
+++ JS_DOM/index.js
@@ -1,6 +1,10 @@
 // add an event listener for all "drum" button
 for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked!");
+    // play a sound
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+
+    this.style.color = "white";
   });
 }
