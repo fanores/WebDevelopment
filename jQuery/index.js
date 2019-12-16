4cdefb3a1265b6754926f53161eb9503bcc6ec26
Stefan Misik <fanores@gmail.com>
2019-12-16 20:53:01 +0100

    jquery: manipulating text of an element with jquery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,8 @@
 // change color of the h1 element, but make sure jQuery is loaded
 $(document).ready(function(){
   $("h1").addClass("big-title margin-50");
+  $("h1").text("Bye");
+
+  // change properties for all buttons
+  $("button").html("<em>Do it!</em>");
 });
