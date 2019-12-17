d8ab4a7231cfaca1e51d5c1611c17ad14c71821d
Stefan Misik <fanores@gmail.com>
2019-12-17 09:48:58 +0100

    challange2: implement next sequence function

--- SimonGame/game.js
+++ SimonGame/game.js
@@ -1,1 +1,11 @@
-alert("Test");
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function gameSequence() {
+  var randomNumber = ( Math.random() * 4 );
+  randomNumber = Math.floor(randomNumber);
+
+  randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+};
