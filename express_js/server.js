c13ff693940698a2ff1dc867a1dde070b27e2677
Stefan Misik <fanores@gmail.com>
2020-03-15 13:59:27 +0100

    express: handling requests and response

--- express_js/server.js
+++ express_js/server.js
@@ -4,7 +4,16 @@
 const express = require("express");
 const app = express();
 
+// handle GET requests
+app.get("/", function(request, response) {
+  // information about request sens
+  // console.log(request);
+
+  // send response to browser
+  response.send("<h1>Hello!</>");
+});
+
 // listen on port 3000 for any HTTP requests
 app.listen(3000, function() {
-  console.log("Server started on port 3000!")
+  console.log("Server started on port 3000!");
 });
