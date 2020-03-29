2cb73ffd10fe78c79f766f56c0b2b144a1fa88f2
Stefan Misik <fanores@gmail.com>
2020-03-29 13:11:27 +0200

    express: processing POST requests

--- express_js_calculator/calculator.js
+++ express_js_calculator/calculator.js
@@ -2,7 +2,11 @@
 
 // require expressjs
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+// you need to specify the use of body-parser explicitely
+app.use(bodyParser.urlencoded({extended: true}));
 
 // handle GET requests
 app.get("/", function(request, response) {
@@ -12,6 +16,18 @@ app.get("/", function(request, response) {
   // send response to browser
   console.log(__dirname);
   response.sendFile(__dirname + "/index.html");
+});
+
+// handle POST requests
+app.post("/", function(request, response) {
+  console.log(request.body);
+  console.log("Number1: " + request.body.num1);
+
+  var num1 = Number(request.body.num1);
+  var num2 = Number(request.body.num2);
+  var result = num1 + num2;
+
+  response.send("Result is: " + result);
 });
 
 // listen on port 3000 for any HTTP requests
