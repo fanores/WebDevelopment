15eafd7fc0f2b9955daa605251139a60d3b29be2
Stefan Misik <fanores@gmail.com>
2020-04-21 20:58:05 +0200

    api: how to parse json

--- api_js/README.md
+++ api_js/README.md
@@ -5,6 +5,7 @@
 2. Joke API: [JokeAPI](https://sv443.net/jokeapi/v2)
 3. Weather API: [OpenWeather](https://openweathermap.org/api)
 4. Chrome Extension: [JSON Viewer Awesome](https://chrome.google.com/webstore/detail/json-viewer-awesome/iemadiahhbebdklepanmkjenfdebfpfe?hl=en)
+5. [Hexadecimal Converter](https://cryptii.com/pipes/hex-to-text)
 
 
 ### List of Commands:
--- api_js/app.js
+++ api_js/app.js
@@ -8,7 +8,26 @@ const app = express();
 app.get("/", function(request, response){
   const nemsovaUrl = "https://api.openweathermap.org/data/2.5/weather?q=Nemsova&appid=01afcb187493871285b239fde270675a&units=metric";
   https.get(nemsovaUrl, function(apiResponse){
-    console.log(apiResponse);
+    console.log(apiResponse.statusCode);
+
+    apiResponse.on("data", function(data) {
+      console.log(data);
+
+      const weatherData = JSON.parse(data);
+      console.log(weatherData);
+
+      const dummyObject = {
+        name: "Test",
+        id: "123"
+      };
+      console.log(JSON.stringify(dummyObject));
+
+      const temperature = weatherData.main.temp;
+      console.log("Temperature is: " + temperature);
+
+      const description = weatherData.weather[0].description;
+      console.log(description);
+    });
   });
   response.send("Server is up and running.");
 });
