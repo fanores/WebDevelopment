cdd99f04809ff762ec32e97e79c187842d7c53cd
Stefan Misik <fanores@gmail.com>
2020-05-01 23:16:43 +0200

    api: use body-parser to parse POST request

--- api_js/app.js
+++ api_js/app.js
@@ -2,12 +2,22 @@
 
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(request, response){
-  const nemsovaUrl = "https://api.openweathermap.org/data/2.5/weather?q=Nemsova&appid=01afcb187493871285b239fde270675a&units=metric";
-  https.get(nemsovaUrl, function(apiResponse){
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(request, response){
+  const selectedCity = request.body.cityName;
+  const apiKey = "01afcb187493871285b239fde270675a";
+  const unit = "metric";
+  const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + selectedCity + "&appid=" + apiKey + "&units=" + unit;
+  https.get(weatherUrl, function(apiResponse){
     console.log(apiResponse.statusCode);
 
     apiResponse.on("data", function(data) {
@@ -32,7 +42,7 @@ app.get("/", function(request, response){
       const weatherIconUrl = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
 
       response.write("<h1>Weather is " + weatherDescription + "<h1>");
-      response.write("<h1>Temperature in Nemsova is: " + temperature + " degrees Celcius.</h1>");
+      response.write("<h1>Temperature in " +  selectedCity + " is: " + temperature + " degrees Celcius.</h1>");
       response.write("<img src=" + weatherIconUrl +">");
       response.send();
     });
@@ -40,5 +50,5 @@ app.get("/", function(request, response){
 });
 
 app.listen(3000, function() {
-  console.log("Server is running on port 3000.")
+  console.log("Server is running on port 3000.");
 });
